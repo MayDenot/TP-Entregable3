fbcb74090ad331c52a342f6009104f01e370c21a
Daniel Gongora Banegas <danielbanegas.gongora@gmail.com>
2025-10-16 15:31:38 -0300

    carrera service,dtos

---
+++ src/main/java/org/example/entregable3/service/DTO/Request/EstudianteRequestDTO.java
@@ -0,0 +1,4 @@
+package org.example.entregable3.service.DTO.Request;
+
+public class EstudianteRequestDTO {
+}
---
+++ src/main/java/org/example/entregable3/service/DTO/Response/EstudianteResponseDTO.java
@@ -0,0 +1,4 @@
+package org.example.entregable3.service.DTO.Response;
+
+public class EstudianteResponseDTO {
+}
